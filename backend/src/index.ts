948f1ca75151ec78be2404d58cf1ae062c747217
samuelpires1999 <samuel.ferpires@gmail.com>
2021-05-11 17:02:37 -0300

    friendship entity setup / redis setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,11 @@ import {UserResolver} from "./resolvers/UserResolver"
 import {createConnection} from 'typeorm'
 import {User} from './entities/User'
 import {Post} from './entities/Post'
+import Redis from 'ioredis'
+import session from 'express-session'
+import cors from 'cors'
+import connectRedis from 'connect-redis'
+import {FriendShip} from './entities/Friendship'
 
 const main = async () => {
     const conn = await createConnection({
@@ -14,13 +19,36 @@ const main = async () => {
         password: '12345',
         database: 'postgres',
         logging: ['query', 'error'],
-        entities: [Post, User],
+        entities: [Post, User, FriendShip],
         synchronize: true
     })
 
     conn.isConnected ? console.log('db up') : console.log('db not up wtf...c')
 
     const app = express()
+
+    const RedisStore = connectRedis(session)
+    const redis = new Redis()
+
+    app.use(cors())
+
+    app.use(session({
+        name: 'qid',
+        store: new RedisStore({
+            client: redis,
+            disableTouch: true,
+        }),
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+            httpOnly:true,
+            sameSite: 'lax',
+            secure: false,
+            domain: undefined,
+        },
+        saveUninitialized: false,
+        secret: 'redistokensecret',
+        resave: false
+    }))
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
